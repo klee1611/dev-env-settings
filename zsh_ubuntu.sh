2eda58338408b70fc35d976346d0169d3cbb5c77
kourtney <kourtneylee1611@gmail.com>
2021-07-11 02:56:29 +0800

    Fix zsh-autosuggestions install

--- zsh_ubuntu.sh
+++ zsh_ubuntu.sh
@@ -10,7 +10,7 @@ sudo apt install fonts-powerline
 
 # Install plugins
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sudo apt install autojump
 
